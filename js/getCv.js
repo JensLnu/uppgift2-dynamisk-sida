387d1e48e7a1e6411aa6b8afd65ce41c6d92eb77
Jens Bengtsson <jb225je@student.lnu.se>
2023-11-16 16:27:23 +0100

    made connections between html and js files

--- js/getCv.js
+++ js/getCv.js
@@ -1,3 +1,3 @@
 function getCvFromJson(){
-    
+    console.log("start getCvFromJson");
 }
--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 window.addEventListener("load", addFunctionality);
 
 function addFunctionality(){
-    
+    console.log("start addFunctionality");
+    const cvBtn = document.getElementById("cv-btn");
+    cvBtn.addEventListener("click", getCvFromJson);
 }
