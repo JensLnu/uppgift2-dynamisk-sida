9542028cbe06e97964f69f812c0c5bff5babdfbe
Jens Bengtsson <jb225je@student.lnu.se>
2023-11-21 16:02:25 +0100

    creating getCv.js script to render cv.json file

--- js/getCv.js
+++ js/getCv.js
@@ -1,3 +1,65 @@
-function getCvFromJson(){
-    console.log("start getCvFromJson");
+const cvModalContentDiv = document.getElementById("created-modal-content");
+
+async function getCvFromJson() {
+    console.log('start getCvFromJson');
+    const respone = await fetch('../json/cv.json');
+    if (respone.ok) {
+        const jsonObj = await respone.json(respone);
+        renderJsonObj(jsonObj);
+    } else {
+        cvModalContentDiv.textContent = 'Error status koden är: ' + respone.status;
+        console.log('Error status koden är: ' + respone.status);
+    }
+}
+
+function renderJsonObj(jsonObj) {
+    // cvModalContentDiv.innerHTML = ''; // gör så att det inte blir dubletter av inläsningen
+
+    // läser in kontaktinformation
+    createNewElement('h3', 'Contact Info');
+    const contactInfoUl = createNewElement('ul', null);
+    createNewElementAppendTo('li', jsonObj.contactInfo.name, contactInfoUl);
+    createNewElementAppendTo('li', jsonObj.contactInfo.email, contactInfoUl);
+    createNewElementAppendTo('li', jsonObj.contactInfo.teleNr, contactInfoUl);
+
+    createNewElement('h3', 'Education');
+
+    // läser in data om chas utbildningen
+    createNewElement('h4', 'Chas Academy');
+    const chas = jsonObj.education.chas;
+    createNewElement('p', chas.title);
+    const chasCoursesUl = createNewElement('ul', null);
+    chas.courses.forEach(content => {
+        createNewElementAppendTo('li', content, chasCoursesUl);
+    });
+    createNewElement('p', chas.dates);
+
+    // läser in data om frikurser utbildning
+    createNewElement('h4', 'Free courses');
+    const freeCourses = jsonObj.education.courses;
+    freeCourses.forEach(course => {
+        createNewElement('h4', `${course.title} (${course.points})`);
+        const CoursesUl = createNewElement('ul', null);
+        course.contents.forEach(content => {
+            createNewElementAppendTo('li', content, CoursesUl);
+        });
+        createNewElement('p', course.dates);
+    });
+    
+    // läser in data om linneuniversitets utbildningen
+    const linneJson = jsonObj.education.linnéuniversity;
+    createNewElement('h4', 'Linnéuniversity');
+}
+
+function createNewElement(element, content) {
+    const newElem = document.createElement(element);
+    newElem.textContent = content;
+    cvModalContentDiv.appendChild(newElem);
+    return newElem
+}
+
+function createNewElementAppendTo(element, content, appendTo) {
+    const newElem = document.createElement(element);
+    newElem.textContent = content;
+    appendTo.appendChild(newElem);
 }
--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 window.addEventListener("load", addFunctionality);
 
 function addFunctionality(){
-    console.log("start addFunctionality");
     const cvBtn = document.getElementById("cv-btn");
     cvBtn.addEventListener("click", getCvFromJson);
 }
