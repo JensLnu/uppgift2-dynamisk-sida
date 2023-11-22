357329d703e18adb790675177794286f98441c63
Jens Bengtsson <jb225je@student.lnu.se>
2023-11-22 08:24:49 +0100

    improved kod by making functions and less "dry"

--- js/getCv.js
+++ js/getCv.js
@@ -1,4 +1,4 @@
-const cvModalContentDiv = document.getElementById("created-modal-content");
+const cvModal = document.getElementById("created-modal-content");
 
 async function getCvFromJson() {
     console.log('start getCvFromJson');
@@ -7,130 +7,97 @@ async function getCvFromJson() {
         const jsonObj = await respone.json(respone);
         renderJsonObj(jsonObj);
     } else {
-        cvModalContentDiv.textContent = 'Error status koden är: ' + respone.status;
+        cvModal.textContent = 'Error status koden är: ' + respone.status;
         console.log('Error status koden är: ' + respone.status);
     }
 }
 
+// -------------------- Main funktion --------------------
+// skapar element med innehållet från cv.json i önskad ordning
 function renderJsonObj(jsonObj) {
-    // cvModalContentDiv.innerHTML = ''; // gör så att det inte blir dubletter av inläsningen
+    // kontrollerar så att inte inläsningen görs flera gånger
+    if (cvModal.textContent == '') {
+        // kontaktinformation section
+        createNewElement('h3', 'Contact Info', cvModal);
+        const contactUl = createNewElement('ul', null, cvModal);
+        createNewElement('li', jsonObj.contactInfo.name, contactUl);
+        createNewElement('li', jsonObj.contactInfo.email, contactUl);
+        createNewElement('li', jsonObj.contactInfo.teleNr, contactUl);
 
-    // läser in kontaktinformation
-    createNewElement('h3', 'Contact Info');
-    const contactInfoUl = createNewElement('ul', null);
-    createNewElementAppendTo('li', jsonObj.contactInfo.name, contactInfoUl);
-    createNewElementAppendTo('li', jsonObj.contactInfo.email, contactInfoUl);
-    createNewElementAppendTo('li', jsonObj.contactInfo.teleNr, contactInfoUl);
+        // education section
+        createNewElement('h3', 'Education', cvModal);
+        createEducationSection('Chas Academy', jsonObj.education.chas);
 
-    createNewElement('h3', 'Education');
-
-    // läser in data om chas utbildningen
-    createHeader('Chas Academy');
-    const chas = jsonObj.education.chas;
-    createNewElement('p', chas.title);
-    const chasCoursesUl = createNewElement('ul', null);
-    chas.courses.forEach(content => {
-        createNewElementAppendTo('li', content, chasCoursesUl);
-    });
-    createNewElement('p', chas.dates);
-
-    // läser in data om frikurser utbildning
-    createHeader('Free courses');
-    const freeCourses = jsonObj.education.courses;
-    freeCourses.forEach(course => {
-        createNewElement('h4', `${course.title} (${course.points})`);
-        const CoursesUl = createNewElement('ul', null);
-        course.contents.forEach(content => {
-            createNewElementAppendTo('li', content, CoursesUl);
+        // läser in data om frikurser utbildning
+        createHeader('Free courses');
+        jsonObj.education.courses.forEach(course => {
+            createNewElement('h4', `${course.title} (${course.points})`, cvModal);
+            createNewElement('p', course.contents.join(', '), cvModal);
+            createNewElement('p', course.dates, cvModal);
         });
-        createNewElement('p', course.dates);
-    });
 
-    // läser in data om linneuniversitets utbildningen
-    createHeader('Linnéuniversity');
-    const linneJson = jsonObj.education.linnéuniversity;
-    linneJson.courses.forEach(course => {
-        createNewElement('h4', course.title);
-        const CoursesUl = createNewElement('ul', null);
-        createNewElementAppendTo('li', `${course.title} (${course.points})`, CoursesUl);
-        createNewElementAppendTo('li', course.contents, CoursesUl);
-    });
-    createNewElement('p', linneJson.dates);
+        // läser in data om linneuniversitets utbildningen
+        createHeader('Linnéuniversity');
+        const linneJson = jsonObj.education.linnéuniversity;
+        linneJson.courses.forEach(course => {
+            createNewElement('h4', `${course.title} (${course.points})`, cvModal);
+            createNewElement('p', course.contents, cvModal);
+        });
+        createNewElement('p', linneJson.dates, cvModal);
 
-    // läser in data om SCoTT utbildningen
-    const scottJson = jsonObj.education.SCoTT;
-    createHeader(scottJson.title);
-    const scottUl = createNewElement('ul', null);
-    scottJson.courses.forEach(content => {
-        createNewElementAppendTo('li', content, scottUl);
-    });
-    createNewElement('p', scottJson.dates);
+        // utbildnings section
+        createEducationSection('SCoTT', jsonObj.education.SCoTT);
+        createEducationSection('Carpentry', jsonObj.education.carpentry);
+        createEducationSection('Upper secondary school', jsonObj.education.gymnasieskola);
 
-    // läser in data om snickar utbildningen
-    const carpentryJson = jsonObj.education.carpentry;
-    createHeader(carpentryJson.title);
-    const carpentryJsonUl = createNewElement('ul', null);
-    carpentryJson.courses.forEach(content => {
-        createNewElementAppendTo('li', content, carpentryJsonUl);
-    });
-    createNewElement('p', carpentryJson.dates);
-
-    // läser in data om snickar utbildningen
-    const gymnasieskolaJson = jsonObj.education.gymnasieskola;
-    createHeader(gymnasieskolaJson.title);
-    const gymnasieskolaJsonUl = createNewElement('ul', null);
-    gymnasieskolaJson.courses.forEach(content => {
-        createNewElementAppendTo('li', content, gymnasieskolaJsonUl);
-    });
-    createNewElement('p', gymnasieskolaJson.dates);
-
-    // erfarenhets sectionen
-    createNewElement('h3', 'Experiences');
-
-    // läser in erfarenheter från bygghjälp
-    createExperienceSection(jsonObj.experiences.bygghjalp);
-    createExperienceSection(jsonObj.experiences.bolgenBygg);
-    createExperienceSection(jsonObj.experiences.skiSeasonVerbier);
-    createExperienceSection(jsonObj.experiences.revelstoke);
-    createExperienceSection(jsonObj.experiences.skistarHemsedal2);
-    createExperienceSection(jsonObj.experiences.skistarHemsedal1);
-    createExperienceSection(jsonObj.experiences.laPoint);
-    createExperienceSection(jsonObj.experiences.skistarTandådalen);
-    createExperienceSection(jsonObj.experiences.klappen);
-    createExperienceSection(jsonObj.experiences.stAnton);
-
-    if (!jsonObj.experiences.stAnton.title){
-
-        console.log("inne i ifsatsen")
+        // erfarenhets sectionen
+        createNewElement('h3', 'Experiences', cvModal);
+        const exp = jsonObj.experiences;
+        createExperienceSection(exp.bygghjalp);
+        createExperienceSection(exp.bolgenBygg);
+        createExperienceSection(exp.skiSeasonVerbier);
+        createExperienceSection(exp.revelstoke);
+        createExperienceSection(exp.skistarHemsedal2);
+        createExperienceSection(exp.skistarHemsedal1);
+        createExperienceSection(exp.laPoint);
+        createExperienceSection(exp.skistarTandådalen);
+        createExperienceSection(exp.klappen);
+        createExperienceSection(exp.stAnton);
     }
 }
 
+// -------------------- Konstruktions funktioner --------------------
 
-function createNewElement(element, content) {
-    const newElem = document.createElement(element);
-    newElem.textContent = content;
-    cvModalContentDiv.appendChild(newElem);
-    return newElem
-}
-
-function createNewElementAppendTo(element, content, appendTo) {
+// parameter: (vilket typ av element, text innehåll, förälder-element)
+function createNewElement(element, content, appendTo) {
     const newElem = document.createElement(element);
     newElem.textContent = content;
     appendTo.appendChild(newElem);
+    return newElem
 }
 
-function createHeader(name) {
-    createNewElement('h4', name);
-    createNewElement('hr', null);
+function createHeader(text) {
+    createNewElement('h4', text, cvModal);
+    createNewElement('hr', null, cvModal);
+}
+
+// parameter: (text innehåll, aktuellt objekt)
+function createEducationSection(header, obj) {
+    createHeader(header);
+    createNewElement('p', obj.title, cvModal);
+    const ul = createNewElement('ul', null, cvModal);
+    obj.courses.forEach(course => {
+        createNewElement('li', course, ul);
+    });
+    createNewElement('p', obj.dates, cvModal);
 }
 
 function createExperienceSection(obj) {
     createHeader(obj.companyName);
-    if (obj.title) createNewElement('p', `${obj.title} in ${obj.location}`);
-    else createNewElement('p', `In ${obj.location}`);
-    const ul = createNewElement('ul', null);
+    obj.title ? createNewElement('p', `${obj.title} in ${obj.location}`, cvModal) : createNewElement('p', `In ${obj.location}`, cvModal);
+    const ul = createNewElement('ul', null, cvModal);
     obj.chores.forEach(chore => {
-        createNewElementAppendTo('li', chore, ul);
+        createNewElement('li', chore, ul);
     });
-    createNewElement('p', obj.dates);
+    createNewElement('p', obj.dates, cvModal);
 }
