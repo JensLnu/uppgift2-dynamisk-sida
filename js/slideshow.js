bcd43ac88fc55e64613641d4b5a3445450bc1793
Jens Bengtsson <jb225je@student.lnu.se>
2023-11-23 21:35:24 +0100

    added closeing function to slideshow

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,7 +1,9 @@
+const slideshowContainer = document.getElementById('slideshow-container');
 const images = document.querySelectorAll('.slideshow-images');
 const leftArrow = document.getElementById('slideshow-btn-left');
 const rightArrow = document.getElementById('slideshow-btn-right');
 const slideshowImage = document.getElementById('slideshow-img');
+const closingTag = document.getElementById('close-tag-slideshow');
 
 let arrayIndex = 0;
 
@@ -11,11 +13,11 @@ images.forEach(img => {
 
 leftArrow.addEventListener("click", moveLeft);
 rightArrow.addEventListener("click", moveRight);
+closingTag.addEventListener("click", closeSlideshow);
 
 function showImageFirstTime(e) {
     console.log('start showImageFirstTime');
 
-    const slideshowContainer = document.getElementById('slideshow-container');
     slideshowContainer.classList.remove('display-none');
     const imagesArray = Object.values(images);
     arrayIndex = imagesArray.indexOf(e.target);
@@ -40,4 +42,9 @@ function moveRight() {
     arrayIndex++;
     if (arrayIndex > images.length - 1) arrayIndex = 0;
     showImage(arrayIndex);
+}
+
+function closeSlideshow() {
+    console.log('start closeSlideshow');
+    slideshowContainer.classList.add('display-none');
 }
