23f2e72155f7d8fc49e7a894dcfcb4de56ea9c3f
Jens Bengtsson <jb225je@student.lnu.se>
2023-11-23 22:27:49 +0100

    tried to solve bug with fetch of json but didnt

--- js/getCv.js
+++ js/getCv.js
@@ -1,12 +1,19 @@
-window.addEventListener("load", addFunctionality);
-
-function addFunctionality() {
-    const cvBtn = document.getElementById("cv-btn");
-    cvBtn.addEventListener("click", getCvFromJson);
-}
 
 // referens till modalen där datan ska presenteras
 const cvModal = document.getElementById("created-modal-content");
+
+function addFunctionality() {
+    console.log('start addFunctionality')
+    const cvBtn = document.getElementById("cv-btn");
+    cvBtn.addEventListener("click", getCvFromJson);
+    // const closeCvBtn = document.getElementById("close-tag-cv-modal");
+    // closeCvBtn.addEventListener("click", () => {
+    //     cvModal.innerHTML = '';
+    // });
+}
+
+// window.addEventListener("load", addFunctionality);
+addFunctionality();
 
 // hämtar json data från cv.json och gör det till ett js objekt
 async function getCvFromJson() {
@@ -14,7 +21,9 @@ async function getCvFromJson() {
     const respone = await fetch('../json/cv.json');
     if (respone.ok) {
         const jsonObj = await respone.json(respone);
-        renderJsonObj(jsonObj);
+        // kontrollerar så att inte inläsningen görs flera gånger
+        if (cvModal.textContent == '') renderJsonObj(jsonObj); // BUGGAAR LITE FIXA SENARE!!---------------------------------------------------------
+
     } else {
         cvModal.textContent = 'Error status koden är: ' + respone.status;
         console.log('Error status koden är: ' + respone.status);
@@ -24,46 +33,43 @@ async function getCvFromJson() {
 // -------------------- Main funktion --------------------
 // skapar element med innehållet från cv.json i önskad ordning
 function renderJsonObj(jsonObj) {
-    // kontrollerar så att inte inläsningen görs flera gånger
-    if (cvModal.textContent == '') { // BUGGAAR LITE FIXA SENARE!!---------------------------------------------------------
-        // kontaktinformation section
-        createNewElement('h3', 'Contact Info', cvModal);
-        const contactUl = createNewElement('ul', null, cvModal);
-        createNewElement('li', jsonObj.contactInfo.name, contactUl);
-        createNewElement('li', jsonObj.contactInfo.email, contactUl);
-        createNewElement('li', jsonObj.contactInfo.teleNr, contactUl);
+    // kontaktinformation section
+    createNewElement('h3', 'Contact Info', cvModal);
+    const contactUl = createNewElement('ul', null, cvModal);
+    createNewElement('li', jsonObj.contactInfo.name, contactUl);
+    createNewElement('li', jsonObj.contactInfo.email, contactUl);
+    createNewElement('li', jsonObj.contactInfo.teleNr, contactUl);
 
-        // utbildnings section
-        createNewElement('h3', 'Education', cvModal);
-        createEducationSection('Chas Academy', jsonObj.education.chas);
+    // utbildnings section
+    createNewElement('h3', 'Education', cvModal);
+    createEducationSection('Chas Academy', jsonObj.education.chas);
 
-        // läser in data om frikurser utbildning
-        createHeader('Free courses');
-        jsonObj.education.courses.forEach(course => {
-            createNewElement('h4', `${course.title} (${course.points})`, cvModal);
-            createNewElement('p', course.contents.join(', '), cvModal);
-            createNewElement('p', course.dates, cvModal);
-        });
+    // läser in data om frikurser utbildning
+    createHeader('Free courses');
+    jsonObj.education.courses.forEach(course => {
+        createNewElement('h4', `${course.title} (${course.points})`, cvModal);
+        createNewElement('p', course.contents.join(', '), cvModal);
+        createNewElement('p', course.dates, cvModal);
+    });
 
-        // läser in data om linneuniversitets utbildningen
-        createHeader('Linnéuniversity');
-        const linneJson = jsonObj.education.linnéuniversity;
-        linneJson.courses.forEach(course => {
-            createNewElement('h4', `${course.title} (${course.points})`, cvModal);
-            createNewElement('p', course.contents, cvModal);
-        });
-        createNewElement('p', linneJson.dates, cvModal);
+    // läser in data om linneuniversitets utbildningen
+    createHeader('Linnéuniversity');
+    const linneJson = jsonObj.education.linnéuniversity;
+    linneJson.courses.forEach(course => {
+        createNewElement('h4', `${course.title} (${course.points})`, cvModal);
+        createNewElement('p', course.contents, cvModal);
+    });
+    createNewElement('p', linneJson.dates, cvModal);
 
-        // utbildnings section
-        createEducationSection('SCoTT', jsonObj.education.SCoTT);
-        createEducationSection('Carpentry', jsonObj.education.carpentry);
-        createEducationSection('Upper secondary school', jsonObj.education.gymnasieskola);
+    // utbildnings section
+    createEducationSection('SCoTT', jsonObj.education.SCoTT);
+    createEducationSection('Carpentry', jsonObj.education.carpentry);
+    createEducationSection('Upper secondary school', jsonObj.education.gymnasieskola);
 
-        // erfarenhets sectionen
-        createNewElement('h3', 'Experiences', cvModal);
-        // skapar en array med objekten i jsonObj.experiences
-        Object.values(jsonObj.experiences).forEach(experience => createExperienceSection(experience));
-    }
+    // erfarenhets sectionen
+    createNewElement('h3', 'Experiences', cvModal);
+    // skapar en array med objekten i jsonObj.experiences
+    Object.values(jsonObj.experiences).forEach(experience => createExperienceSection(experience));
 }
 
 // -------------------- Konstruktions funktioner --------------------
